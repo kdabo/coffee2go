82e324d4e3453ea9ee0173224898df8b349ef32b
katarina <k.dabo@dtg.nl>
2019-04-09 17:10:36 +0200

    form

---
+++ src/Form.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+
+export interface IValues {
+    [key: string]: any;
+}
+
+interface IFieldProps {
+    name: string;
+    label: string;
+    type?: "Text" | "Email" | "Select" | "Textarea" ;
+    options?: string[]
+}
+
+interface IFormProps {
+    defaultValues: IValues
+}
+
+interface IState {
+    values: IValues
+}
+
+export class Form extends React.Component<IFormProps, IState> {
+    constructor(props: IFormProps) {
+        super(props);
+
+        this.state = {
+            values: props.defaultValues
+        }
+    }
+
+    public static Field: React.SFC<IFieldProps> = props => {
+
+        const { name, label, type, options } = props;
+
+        return (
+            <div className="form-group">
+                <label htmlFor={name}>{label}</label>
+                {(type === "Text" || type === "Email") && <input type={type.toLowerCase()} id={name}/>}
+                {type === "Textarea" && (<textarea id={name}/>)}
+                {type === "Select" && <select>
+                    {options && options.map(option => (
+                        <option key={option} value={option}>{option}</option>
+                    ))}
+                </select>}
+            </div>
+        )
+    };
+
+    public render() {
+        return(
+            <form className="form" noValidate={true}>
+                {this.props.children}
+            </form>
+        )
+    }
+}
+
+Form.Field.defaultProps = {
+    type: "Text"
+};
